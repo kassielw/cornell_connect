d713594bd60f08664c3f18b6625bcba2d2c08429
Hannah Lee <hannahlee@dhcp-vl2041-10699.eduroam.cornell.edu>
2021-05-04 20:55:17 -0400

    Set up databases and attraction and post routes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "hack_challenge.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -26,101 +26,89 @@ def failure_response(message, code=404):
     return json.dumps({"success": False, "error": message}), code
 
 
-@app.route("/")
-@app.route("/api/courses/")
-def get_courses():
-    return success_response([c.serializeSome() for c in Course.query.all()])
+# retrieve all attractions
+@app.route("/attractions/")
+def get_attractions():
+    return success_response([a.serialize() for a in Attraction.query.all()])
 
+# retrieve an attraction
+@app.route("/attractions/<int:id>/")
+def get_attraction(id):
+    attraction = Attraction.query.filter_by(id=id).first()
+    if not attraction:
+        return failure_response("Attraction not found")
+    return success_response(attraction.serialize())
 
-@app.route("/api/courses/", methods=["POST"])
-def create_course():
-    body = json.loads(request.data)
-    new_course = Course(code=body.get("code"), name=body.get("name"))
-    if not new_course.code or not new_course.name:
-        return failure_response("Missing required field")
-    db.session.add(new_course)
+# create attraction
+@app.route("/attractions/", methods=["POST"])
+def create_attraction():
+  body = json.loads(request.data)
+      new_attraction = Attraction(name=body.get("name"), address=body.get(
+          "address"), description=body.get("description"))
+       if not new_attraction.name or not new_attraction.address or not new_attraction.description:
+            return failure_response("Missing required field")
+        db.session.add(new_attraction)
+        db.session.commit()
+        return success_response(new_attraction.serialize(), 201)
+
+# delete attraction
+@app.route("/attractions/<int:a_id>/", methods=["DELETE"])
+def delete_attraction(a_id):
+    attraction = Attraction.query.filter_by(id=a_id).first()
+    if not attraction:
+        return failure_response("Attraction not found")
+    db.session.delete(attraction)
     db.session.commit()
-    return success_response(new_course.serialize(), 201)
+    return success_response(attraction.serialize())
 
+# retrieve all posts for a given attraction
+@app.route("/posts/attraction/<int:a_id>/")
+def get_posts(a_id):
+    attraction = Attraction.query.filter_by(id=a_id).first()
+    if not attraction:
+        return failure_response("Attraction not found")
+    posts = Post.query.filter_by(attraction=a_id).all()
+    if not posts:
+        return failure_response("Posts not found")
+    return success_response([p.serialize() for p in posts], 201)
 
-@app.route("/api/courses/<int:course_id>/")
-def get_course(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found")
-    return success_response(course.serializeSome())
+# retrieve a post
+@app.route("/posts/<int:id>/")
+def get_post(id):
+    post = Post.query.filter_by(id=id).first()
+    if not post:
+        return failure_response("Post not found")
+    return success_response(post.serialize())
 
+# create post (add post to attraction)
+@app.route("/posts/attraction/<int:a_id>/", methods=["POST"])
+def create_post(a_id):
+  body = json.loads(request.data)
+  attraction = Attraction.query.filter_by(id=a_id).first()
+        if not attraction:
+            return failure_response("Attraction not found")
+      new_post = Post(netid=body.get("netid"), name=body.get(
+          "name"), picture=body.get("picture"), rating=body.get("rating"), description=body.get("description"), attraction=a_id)
+       if not new_post.netid or not new_post.name or not new_post.description:
+            return failure_response("Missing required field")
+        db.session.add(new_post)
+        db.session.commit()
+        return success_response(new_post.serialize(), 201)
 
-@app.route("/api/courses/<int:course_id>/", methods=["DELETE"])
-def delete_course(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found")
-    db.session.delete(course)
-    db.session.commit()
-    return success_response(course.serializeSome())
+# ZACH TODO: edit post (ensure that user netid is same as previous)
+@app.route("/posts/edit/<int:id>/", methods=["POST"])
 
+# ZACH TODO: retrieve all comments for a given post
+@app.route("/comments/<int:p_id>/")
 
-@app.route("/api/users/", methods=["POST"])
-def create_user():
-    body = json.loads(request.data)
-    new_user = User(name=body.get("name"), netid=body.get("netid"))
-    if not new_user.name or not new_user.netid:
-        return failure_response("Missing required field")
-    db.session.add(new_user)
-    db.session.commit()
-    return success_response(new_user.serialize(), 201)
+# ZACH TODO: create comment (add comment to post)
+@app.route("/comments/<int:p_id>/", methods=["POST"])
 
+# ZACH TODO: edit comment (ensure that user netid is same as previous)
+@app.route("/comments/edit/<int:id>/", methods=["POST"])
 
-@app.route("/api/users/<int:user_id>/")
-def get_user(user_id):
-    user = User.query.filter_by(id=user_id).first()
-    if user is None:
-        return failure_response("User not found")
-    return success_response(user.serialize())
-
-
-@app.route("/api/courses/<int:course_id>/add/", methods=["POST"])
-def add_user_to_course(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found")
-    body = json.loads(request.data)
-    user_id = body.get("user_id")
-    user_type = body.get("type")
-    if not user_id or not user_type:
-        return failure_response("Missing required field")
-    user = User.query.filter_by(id=user_id).first()
-    if user is None:
-        return failure_response("User not found")
-    if body.get("type") == "student":
-        course.students.append(user)
-    elif body.get("type") == "instructor":
-        course.instructors.append(user)
-    elif body.get("type") != "student" or body.get("type") != "instructor":
-        return failure_response("Invalid user type")
-    db.session.commit()
-    return success_response(course.serializeSome())
-
-
-@app.route("/api/courses/<int:course_id>/assignment/", methods=["POST"])
-def create_assignment(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found")
-    body = json.loads(request.data)
-    new_assignment = Assignment(
-        title=body.get("title"),
-        due_date=body.get("due_date"),
-        course_id=course_id
-    )
-    if not new_assignment.title or not new_assignment.due_date:
-        return failure_response("Missing requried field")
-    db.session.add(new_assignment)
-    db.session.commit()
-    return success_response(new_assignment.serialize())
-
-
+# notifcation system (reach goal): given netid, retrieve posts and comments
+# @app.route("/posts/<int:a_id>/")
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
--- db.py
+++ db.py
@@ -7,20 +7,20 @@ class Attraction(db.Model):
     __tablename__ = "attraction"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    location = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     posts = relationship("Post", back_populates("attraction"))
 
     def __init__(self, **kwargs):
         self.name = kwargs.get("name")
-        self.location = kwargs.get("location")
+        self.address = kwargs.get("address")
         self.description = kwargs.get("description")
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
-            "location": self.location,
+            "address": self.address,
             "description": self.description,
             "posts": [p.serialize() for p in self.posts]
         }
@@ -32,7 +32,7 @@ class Post(db.Model):
     netid = db.Column(db.String, nullable=False)
     name = db.Column(db.String, default="Anonymous")
     picture = db.Column(db.String, nullable=True)
-    rating = db.Column(db.String, default="N/A")
+    rating = db.Column(db.Integer,  nullable=True)
     description = db.Column(db.String, nullable=False)
     comments = relationship("Comment", back_populates("post"))
     attraction = relationship("Attraction", back_populates("posts"))
@@ -48,20 +48,20 @@ class Post(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "picture": self.picture,
-            "rating": self.rating,
+            "picture": self.picture if self.picture else "N/A",
+            "rating": self.rating if self.rating else "N/A",
             "description": self.description,
             "comments": [c.serialize() for c in self.comments]
         }
 
-    def serialize_some(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "rating": self.rating,
-            "description": self.description,
-            "comments": [c.serialize() for c in self.comments]
-        }
+    # def serialize_some(self):
+    #     return {
+    #         "id": self.id,
+    #         "name": self.name,
+    #         "rating": self.rating,
+    #         "description": self.description,
+    #         "comments": [c.serialize() for c in self.comments]
+    #     }
 
 
 class Comment(db.Model):
