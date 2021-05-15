64b1c533c7a4da4de430dfcb17efb7edd18f535f
Varsha <Varsha@dhcp-vl2042-22377.redrover.cornell.edu>
2021-05-14 22:08:25 -0400

    Removed frontend

--- frontend/Extensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Extensions.swift
-//  HackChallenge_
-//
-//  Created by Varsha Iyer on 5/6/21.
-//
-
-import Foundation
-extension UINavigationController
--- frontend/NetworkManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkManager.swift
-//  HackChallenge_
-//
-//  Created by Varsha Iyer on 5/10/21.
-//
-
-import Foundation
